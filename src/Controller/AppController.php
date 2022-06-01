624f43659921876bbd8a70fce21e2d96cfa30b76
Louis Perrenot <louis.perrenot@edu.devinci.net>
2022-06-01 23:29:03 +0200

    rebase controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/app')]
+#[Route('/')]
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
